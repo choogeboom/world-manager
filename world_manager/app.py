1c044d36a9b345e6032f2f76d93fd402dc3d716f
hoogamaphone <chris.hoogeboom@gmail.com>
2017-09-21 00:37:09 -0400

    implemented the app factory method

--- world_manager/app.py
+++ world_manager/app.py
@@ -1,13 +1,51 @@
+from typing import Optional
+
+from flask import Flask
+
 from world_manager.extensions import db
 
 
 ACTIVE_EXTENSIONS = [db]
+ACTIVE_BLUEPRINTS = []
 
 
-def create_app(settings_override: dict=None):
+def create_app(settings_override: Optional[dict]=None) -> Flask:
     """
     Create a Flask app
 
     :param settings_override: any settings to override
     :return: flask app
     """
+
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_object('config.settings')
+    app.config.from_pyfile('settings.py', silent=True)
+
+    if settings_override:
+        app.config.update(settings_override)
+
+    initialize_extensions(app)
+    register_blueprints(app)
+
+    return app
+
+
+def initialize_extensions(app: Flask) -> None:
+    """
+    Initialize extensions
+
+    :param app: The flask app
+    """
+    for extension in ACTIVE_EXTENSIONS:
+        extension.init_app(app)
+
+
+def register_blueprints(app: Flask) -> None:
+    """
+    Register blueprints
+
+    :param app: the flask app
+    """
+    for blueprint in ACTIVE_BLUEPRINTS:
+        app.register_blueprint(blueprint)
