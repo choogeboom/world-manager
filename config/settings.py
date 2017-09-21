28d7ad237fff867b9b46aa1c73e56f822f0265fc
choogeboom <chris.hoogeboom@gmail.com>
2017-09-21 15:49:45 -0400

    updated settings

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,7 @@
 DEBUG = True
 
+SERVER_NAME = 'localhost:5000'
 SECRET_KEY = 'insecurekeyfordev'
 
 SQLALCHEMY_DATABASE_URI = 'sqlite+pysqlite:///world_manager.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
