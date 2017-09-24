4eceede220b13507afa66698b11d2e2af0ad270f
hoogamaphone <chris.hoogeboom@gmail.com>
2017-09-23 22:57:21 -0400

    continued filling in stat blocks.

--- world_manager/model/stat.py
+++ world_manager/model/stat.py
@@ -1,3 +1,4 @@
+import enum
 
 from utils.sql import ResourceMixin
 from world_manager.extensions import db
@@ -98,11 +99,102 @@ class StatBlockClass(ResourceMixin, db.Model):
     stat_block_id = db.Column(db.Integer, db.ForeignKey('stat_block.id'))
     class_id = db.Column(db.Integer, db.ForeignKey('class.id'))
     level = db.Column(db.Integer, nullable=False, index=True)
+    hit_die = db.Column(db.Enum(['d4', 'd6', 'd8', 'd10', 'd12', 'd20', 'd100']))
     class_ = db.relationship('Class')
+
+
+class Background(ResourceMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, nullable=False, index=True)
+    description = db.Column(db.String)
+
+
+class Race(ResourceMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, nullable=False, index=True)
+    description = db.Column(db.String)
+    creature_type_id = db.Column(db.Integer, db.ForeignKey('creature_type.id'))
+    creature_type = db.relationship('CreatureType')
+
+
+class CreatureType(ResourceMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, nullable=False, index=True)
+    description = db.Column(db.String)
+
+
+class Alignment(enum.Enum):
+    Neutral = 1
+    ChaoticNeutral = 2
+    ChaoticGood = 3
+    NeutralGood = 4
+    LawfulGood = 5
+    LawfulNeutral = 6
+    LawfulEvil = 7
+    NeutralEvil = 8
+    ChaoticEvil = 9
 
 
 class StatBlock(ResourceMixin, db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256), nullable=False)
+    name = db.Column(db.String(256), nullable=False, index=True)
     classes = db.relationship('StatBlockClass')
+    background_id = db.Column(db.Integer, db.ForeignKey('background.id'),
+                              nullable=True, index=True)
+    background = db.relationship('Background')
+    race_id = db.Column(db.Integer, db.ForeignKey('race.id'),
+                        nullable=False, index=True)
+    race = db.relationship('Race')
+    alignment = db.Column(db.Enum(Alignment), index=True)
+    experience_points = db.Column(db.Integer, index=True, server_default=0)
+
+    proficiency_bonus = db.Column(db.Integer)
+    strength = db.Column(db.Integer)
+    dexterity = db.Column(db.Integer)
+    constitution = db.Column(db.Integer)
+    intelligence = db.Column(db.Integer)
+    wisdom = db.Column(db.Integer)
+    charisma = db.Column(db.Integer)
+
+    base_hit_point_max = db.Column(db.Integer)
+
+    walking_speed = db.Column(db.Integer)
+    swimming_speed = db.Column(db.Integer)
+    climbing_speed = db.Column(db.Integer)
+    flying_speed = db.Column(db.Integer)
+
+    strength_save_proficiency = db.Column(db.Boolean, server_default=False)
+    athletics_proficiency = db.Column(db.Boolean, server_default=False)
+
+    dexterity_save_proficiency = db.Column(db.Boolean, server_default=False)
+    acrobatics_proficiency = db.Column(db.Boolean, server_default=False)
+    sleight_of_hand_proficiency = db.Column(db.Boolean, server_default=False)
+    stealth_proficiency = db.Column(db.Boolean, server_default=False)
+
+    constitution_save_proficiency = db.Column(db.Boolean,  server_default=False)
+
+    intelligence_save_proficiency = db.Column(db.Boolean, server_default=False)
+    arcana_proficiency = db.Column(db.Boolean, server_default=False)
+    history_proficiency = db.Column(db.Boolean, server_default=False)
+    investigation_proficiency = db.Column(db.Boolean, server_default=False)
+    nature_proficiency = db.Column(db.Boolean, server_default=False)
+    religion_proficiency = db.Column(db.Boolean, server_default=False)
+
+    wisdom_save_proficiency = db.Column(db.Boolean, server_default=False)
+    animal_handling_proficiency = db.Column(db.Boolean, server_default=False)
+    insight_proficiency = db.Column(db.Boolean, server_default=False)
+    medicine_proficiency = db.Column(db.Boolean, server_default=False)
+    perception_proficiency = db.Column(db.Boolean, server_default=False)
+    survival_proficiency = db.Column(db.Boolean, server_default=False)
+
+    charisma_save_proficiency = db.Column(db.Boolean, server_default=False)
+    deception_proficiency = db.Column(db.Boolean, server_default=False)
+    intimidation_proficiency = db.Column(db.Boolean, server_default=False)
+    performance_proficiency = db.Column(db.Boolean, server_default=False)
+    persuasion_proficiency = db.Column(db.Boolean, server_default=False)
+
+    personality_traits = db.Column(db.String)
+    ideals = db.Column(db.String)
+    bonds = db.Column(db.String)
+    flaws = db.Column(db.String)
