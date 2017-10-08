1de611d6b71856375f0808fc833c12ec238ade43
hoogamaphone <chris.hoogeboom@gmail.com>
2017-10-08 01:34:33 -0400

    added a celery app factory method

--- world_manager/app.py
+++ world_manager/app.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+from celery import Celery, Task
 from flask import Flask
 
 from world_manager.extensions import db, debug_toolbar, jsglue
@@ -9,6 +10,38 @@ from world_manager.blueprints.page.views import page
 
 ACTIVE_EXTENSIONS = [db, debug_toolbar, jsglue]
 ACTIVE_BLUEPRINTS = [page]
+CELERY_TASK_LIST = []
+
+
+def create_celery_app(app: Optional[Flask]=None) -> Celery:
+    """
+    Create a new Celery app and tie the app's config together with celery's.
+    Wrap all tasks in the context of the application.
+
+    :param app: The flask app
+    :return: celery app
+    """
+
+    app = app or create_app()
+
+    class ContextTask(Task):
+
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return super().__call__(self, *args, **kwargs)
+
+        def run(self, *args, **kwargs):
+            """The body of the task executed by workers."""
+            raise NotImplementedError('Tasks must define the run method.')
+
+    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'],
+                    include=CELERY_TASK_LIST)
+
+    celery.conf.update(app.config)
+    # noinspection PyPropertyAccess
+    celery.Task = ContextTask
+
+    return celery
 
 
 def create_app(settings_override: Optional[dict]=None) -> Flask:
