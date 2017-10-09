8f3eb90ce3929e7f4593cad0a5a5bceb05ed40f9
hoogamaphone <chris.hoogeboom@gmail.com>
2017-10-09 15:12:59 -0400

    made ContactForm a subclass of FlaskForm instead of Form.

--- world_manager/blueprints/contact/forms.py
+++ world_manager/blueprints/contact/forms.py
@@ -1,10 +1,10 @@
-from flask_wtf.form import Form
+from flask_wtf.form import FlaskForm
 from wtforms.fields.simple import TextAreaField
 from wtforms.validators import DataRequired, Length
 from wtforms_components import EmailField
 
 
-class ContactForm(Form):
+class ContactForm(FlaskForm):
     email = EmailField("What's your e-mail address?",
                        [DataRequired(), Length(3, 254)])
     message = TextAreaField("What's your question or issue?",
